804b8815267d5b955508ea4284fbc2f4909dbc0a
Stefan Benz <stefan@caos.ch>
2023-11-29 13:25:34 +0100

    docs: v2.41.x to v2.42.x

---
+++ fill-projections/v2.41.x-to-v2.42.x/users-projections.sql
@@ -0,0 +1,100 @@
+BEGIN;
+
+INSERT INTO projections.projects4
+SELECT id, creation_date, change_date, sequence, state, resource_owner, instance_id, name, project_role_assertion, project_role_check, has_project_check, private_labeling_setting
+FROM projections.projects3
+WHERE owner_removed = false
+    ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.instance_members4
+SELECT creation_date, change_date, user_id, user_resource_owner, roles, sequence, resource_owner, instance_id, id
+FROM projections.instance_members3
+WHERE owner_removed = false
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.org_members4
+SELECT creation_date, change_date, user_id, user_resource_owner, roles, sequence, resource_owner, instance_id, org_id
+FROM projections.org_members3
+WHERE owner_removed = false
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.project_grant_members4
+SELECT creation_date, change_date, user_id, user_resource_owner, roles, sequence, resource_owner, instance_id, project_id, grant_id, granted_org
+FROM projections.project_grant_members3
+WHERE owner_removed = false
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.project_grants4
+SELECT grant_id, creation_date, change_date, sequence, state, resource_owner, instance_id, project_id, granted_org_id, granted_role_keys
+FROM projections.project_grants3
+WHERE owner_removed = false
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.project_members4
+SELECT creation_date, change_date, user_id, user_resource_owner, roles, sequence, resource_owner, instance_id, project_id
+FROM projections.project_members3
+WHERE owner_removed = false
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.project_roles4
+SELECT project_id, role_key, creation_date, change_date, sequence, resource_owner, instance_id, display_name, group_name
+FROM projections.project_roles3
+WHERE owner_removed = false
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.instance_members4
+SELECT creation_date, change_date, user_id, user_resource_owner, roles, sequence, resource_owner, instance_id, id
+FROM projections.instance_members3
+WHERE owner_removed = false
+ON CONFLICT DO NOTHING;
+
+
+INSERT INTO projections.current_states
+SELECT 'projections.projects4', instance_id, last_updated, aggregate_id, aggregate_type, sequence, event_date, position
+FROM projections.current_states
+WHERE projection_name = 'projections.projects3'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.current_states
+SELECT 'projections.instance_members4', instance_id, last_updated, aggregate_id, aggregate_type, sequence, event_date, position
+FROM projections.current_states
+WHERE projection_name = 'projections.instance_members3'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.current_states
+SELECT 'projections.org_members4', instance_id, last_updated, aggregate_id, aggregate_type, sequence, event_date, position
+FROM projections.current_states
+WHERE projection_name = 'projections.org_members3'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.current_states
+SELECT 'projections.project_grant_members4', instance_id, last_updated, aggregate_id, aggregate_type, sequence, event_date, position
+FROM projections.current_states
+WHERE projection_name = 'projections.project_grant_members3'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.current_states
+SELECT 'projections.project_grants4', instance_id, last_updated, aggregate_id, aggregate_type, sequence, event_date, position
+FROM projections.current_states
+WHERE projection_name = 'projections.project_grants3'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.current_states
+SELECT 'projections.project_members4', instance_id, last_updated, aggregate_id, aggregate_type, sequence, event_date, position
+FROM projections.current_states
+WHERE projection_name = 'projections.project_members3'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.current_states
+SELECT 'projections.project_roles4', instance_id, last_updated, aggregate_id, aggregate_type, sequence, event_date, position
+FROM projections.current_states
+WHERE projection_name = 'projections.project_roles3'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO projections.current_states
+SELECT 'projections.instance_members4', instance_id, last_updated, aggregate_id, aggregate_type, sequence, event_date, position
+FROM projections.current_states
+WHERE projection_name = 'projections.instance_members3'
+ON CONFLICT DO NOTHING;
+
+COMMIT;
